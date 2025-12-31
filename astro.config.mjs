5e5e1802686091d4c81671dea9d9b26afc425a33
Tayyab Sajjad <tayyabsajjad4679@gmail.com>
2025-12-31 17:06:32 +0500

    astrojs installed

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,14 @@
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel/serverless';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
+  output: 'hybrid',
+  adapter: vercel(),
   site: 'https://hashturn.com',
   integrations: [react()],
 
